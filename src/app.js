bca10a5005e2330859e822fd64ab157b3d3b0ca1
Md Mirza Shihab <msmd2@graduate.utm.my>
2024-11-07 17:35:33 +0600

    Test route(/api/test) created to check if the server CICD is working fine or not

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.json());
 app.use("/api/posts", contentRouter);
+app.use("/api/test", (req, res) => {
+  res.send({ message: "This is a test route to check if the server CICD is working fine or not" });
+});
+
 
 app.get("/", (req, res) => {
   res.send({ message: "Welcome to our TechPostGen API Server" });
